9776a420697ba748b7e5d343b44b37e2b8962de0
superpuper777 <superfevrik@gmail.com>
2023-01-24 00:39:37 +0300

    refactor: add unsaved changes

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,6 +1,5 @@
 import { mouse, left, right, up, down } from "@nut-tree/nut-js";
 
-let command: string, x: number;
 
 export const mousemoveUp = (x: any) => {
     (async () => {
@@ -26,23 +25,3 @@ export const mousemoveLeft = (x: any) => {
 };
 
 
-export const movementCommand = (data: any) => {   
-    [ command,  x ] = data.split(" ");   
-    return `${command} {${x} px}`;
-}
-
-
-export const switchCommands = async () => {
-    switch (command) {
-        case "mouse_up":
-            return mousemoveUp(x);
-        case "mouse_down":
-            return mousemoveDown(x);
-        case "mouse_left":
-            return mousemoveLeft(x);
-        case "mouse_right":
-            return mousemoveRight(x);
-        default:
-            break;
-    }
-}
