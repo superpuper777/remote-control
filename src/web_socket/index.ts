d2b720fdbe0393102e0b7021753aca2aa8e2a8b3
superpuper777 <superfevrik@gmail.com>
2023-01-23 23:17:36 +0300

    refactor: connect to websocket, close wss

--- src/web_socket/index.ts
+++ src/web_socket/index.ts
@@ -1,8 +1,15 @@
-import { WebSocketServer } from 'ws';
+import { WebSocketServer, createWebSocketStream } from "ws";
+import { mouse } from "@nut-tree/nut-js";
 
 export const startWebsocket = (HTTP_PORT: number) => {
-        const PORT = HTTP_PORT || 8080;
-        console.log('WebSocketServer is running on port', PORT); 
+  const PORT = HTTP_PORT || 8080;
+  const wss = new WebSocketServer({ port: PORT });
+  wss.on("connection", (ws, req) => {
+    console.log(`Client connected`);
+  });
+
+  process.on("SIGINT", () => {
+    wss.close();
+    console.log("WebSocketServer is stopped");
+  });
 };
-
-
